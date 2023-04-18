d6d33a9c77564919f99b56d95423abaaca5816cb
arshneetkathuria <96715617+SriDev03@users.noreply.github.com>
2023-04-18 18:09:54 +0530

    React initialised

---
+++ dyslexia_ai/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dyslexia Art with AI</h1>
+    </div>
+  );
+}
+
+export default App;
